5716e34483ec3f18ae260374a1dae3f4354b3f4e
Matthieu96Code <senoumatthhieu96@gmail.com>
2023-06-01 03:31:41 +0000

    Load music album data

--- classes/load_data.rb
+++ classes/load_data.rb
@@ -1,7 +1,13 @@
 require 'json'
 require 'fileutils'
 
-require_relative 'models/load_game_mod'
+require_relative '../models/load_game_mod'
+require_relative '../models/load_author_mod'
+require_relative '../models/load_music_album_mod'
 
+# Class for loading all data from json files.
 class LoadData
+  include LoadGame
+  include LoadAuthor
+  include LoadMusicAlbum
 end
