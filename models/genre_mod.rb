2cfc884d42c424b8ec3e5184550db8a0eadfb685
Matthieu96Code <senoumatthhieu96@gmail.com>
2023-06-01 14:14:41 +0000

    Create add_genre and display genre in module

---
+++ models/genre_mod.rb
@@ -0,0 +1,15 @@
+module GenreMod
+  def add_genre
+    print 'Enter genre name: '
+    name = gets.chomp
+    genre = Genre.new(name)
+    @genres.push(genre)
+    SaveData.new.save_genre(@genres)
+  end
+
+  def display_genres
+    @genres.each do |genre|
+      puts "Name: #{genre.name}"
+    end
+  end
+end
