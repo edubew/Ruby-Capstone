39ff7ad09d965aa979883e32074a8aa6499142ba
Matthieu96Code <senoumatthhieu96@gmail.com>
2023-06-01 13:50:48 +0000

    Add genre class properties

--- classes/genre.rb
+++ classes/genre.rb
@@ -1,9 +1,11 @@
 
 # Genre class for adding Genres name
 class Genre
+  attr_reader :name
 
-  def initialize()
+  def initialize(name)
     @id = Random.rand(1..1_000_000)
+    @name = name
     @items = []
   end
 
